c2b14401b0f22709af13aded060ac98f2472d4b9
wekt2k04 <k.tsetse0026@uca.ac.ma>
2025-08-23 00:56:16 +0100

    Update: Improve item management functionality

--- functionality/add_item.py
+++ functionality/add_item.py
@@ -3,42 +3,47 @@ import time
 
 def add_item(item, chores:list):
   """
-  Manages the process of adding a single item to the to-do list.
-  
+  Add a single item to the to-do list.
+
   Args:
     Name(str): The name of the item (required).
-  
+
   Returns:
     None.
   """
-  new_item = {"name": item, "status":"pending"}
+  new_item = {"name": item, "status": "pending"}
   chores.append(new_item)
 
 def request_to_add_item(chores:list):
   """
-  This function manages the interaction with the user for adding an item.
+  Manages the interaction with the user for adding an item.
 
   Args:
-    chores(list): the list of chores, each chore as a dictionnary (required).
+    chores(list): the list of chores, each chore as a dictionary (required).
   
   Returns:
     None
   """
   while True:
-    user_input_1 = input(">> Would you like to add a chore to the list ? [y/n] :")
-    if user_input_1.lower() == 'y':
-      user_input_2 = input(">> And which chore would you like to complete today ?\n==> ")
-      add_item(user_input_2, chores=chores)
-      print(">> Chore added successfully !\n")
-      display_specific_items(chores=chores, status="All")
-      time.sleep(2)
-      return
-    elif user_input_1.lower() == 'n': 
-      print(">> Okay ! Let's go back to the menu...")
-      time.sleep(3)
-      return
-    else:
-      print("ERROR : Please write a required letter.")
+    try:
+      user_input_1 = input(">> Would you like to add a chore to the list ? [y/n]: ")
+      user_input_1.lower()
+      if user_input_1 not in ['y', 'n']:
+        raise ValueError("ERROR: Please enter a required letter.")
+      elif user_input_1 == 'y':
+        user_input_2 = input(">> And what do you plan to do ?\n==> ")
+        add_item(user_input_2, chores=chores)
+        print(">> Activity added successfully ! \n")
+        time.sleep(2)
+        display_specific_items(chores=chores, status="all")
+        time.sleep(2)
+        return
+      else:
+        print(">> Okay ! Let's go back to the menu...")
+        time.sleep(2)
+        return
+    except ValueError as e:
+      print(e)
 
 if __name__ == "__main__":
   request_to_add_item()
--- functionality/salutations.py
+++ functionality/salutations.py
@@ -1,29 +1,30 @@
 import time
+
 def show_menu():
   print("\n===Menu===")
   print("1. Display to-do list")
-  print("2. Add a chore")
-  print("3. Mark a chore as completed")
-  print("4. Remove a chore")
+  print("2. Add an activity")
+  print("3. Mark an activity as completed")
+  print("4. Remove an activity")
   print("5. Exit", end="\n\n")
 
 def first_welcome_user():
   while True:
-    print(">>Our functionalities :")
+    print("Our functionnalities :")
     show_menu()
     try:
-      user_input = int(input(">>Tell us do you want to do :\n===> "))
+      user_input = int(input(">>Tell us what you want to do:\n==> "))
       if user_input in list(range(1, 6)):
         return str(user_input)
       else:
-        raise ValueError("The number should be between 1 and 5!")
-    except ValueError:
-      print("ERROR : Please the entered number should be between 1 and 5")
+        raise ValueError("ERROR: The entered number should be an integer between 1 and 5!")
+    except ValueError as e:
+      print(e)
       time.sleep(2)
-    except Exception:
-      print("ERROR : Please enter a number !")
+    except Exception as e:
+      print(f"ERROR: {e}")
       time.sleep(2)
-
 
 def goodbye_message():
   print("\n\n=======================================================================================\nIt has been a pleasure to interact with you, see you for your next day-to-day tasks!\n=======================================================================================\n")
+  time.sleep(5)
